d47d3d94d63d9597f3e10cc57fdbbe5e37286112
Quertte <a.injen90@gmail.com>
2022-08-10 14:43:46 +0300

    factorial

---
+++ factorial.js
@@ -0,0 +1,14 @@
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+function factorialRec(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorialRec(n - 1);
+}
